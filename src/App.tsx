5c93c20960180217dd554ff482175cc3bf5777db
+ <+>
2023-07-18 02:43:50 +0300

    creating the nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import Navbar from "./components/Navbar/Navbar"
 
 
 const App = () => {
 
   return (
     <>
-      <div>App</div>
+      <div className="app">
+        <Navbar />
+      </div>
     </>
   )
 }
