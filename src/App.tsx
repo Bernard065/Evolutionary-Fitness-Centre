788a8f1ac4a944262ef4d8e5307d556fda89faa5
+ <+>
2023-07-19 10:41:06 +0300

    adding a functionality on the page to check if navbar has been scrolled

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Navbar from "./components/Navbar/Navbar"
 import { SelectedPage } from "./types"
 
 
 const App = () => {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.screenY === 0) {
+        setIsTopPage(true);
+        setSelectedPage(SelectedPage.Home);
+      } else {
+        setIsTopPage(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
 
   return (
     <>
       <div className="app">
         <Navbar  
+          isTopOfPage={isTopOfPage}
           selectedPage={selectedPage}
           setSelectedPage={setSelectedPage} 
         />
