dc54ea138b8a0e15622111bc62a2425c1782e058
+ <+>
2023-07-19 09:32:11 +0300

    adding the close icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { SelectedPage } from '@/types';
 import Logo from '../../assets/Logo.png'
 import Link from './Link';
 import useMediaQuery from '@/hooks/useMediaQuery';
-import { Bars3Icon } from '@heroicons/react/24/solid';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 import { useState } from 'react';
 import ActionButton from '../ActionButton/ActionButton';
 
@@ -67,7 +67,14 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
 
      {/* Mobile Menu Modal */}
      {!isAboveMediaScreens && isMenuToggled && (
-      <div className='fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl'></div>
+      <div className='fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl'>
+        {/* Close Icon */}
+        <div className='flex justify-end p-12'>
+          <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+            <XMarkIcon className='h-6 w-6 text-gray-400' />
+          </button>
+        </div>
+      </div>
      )}
     </nav>
   )
